aaad9f04bfd7cc9b14642f8f7098fd47c04ecd99
sgnpdt <44494209+sgnpdt@users.noreply.github.com>
2019-06-05 11:06:35 +0700

    Fix PIN code

---
+++ common.js
@@ -0,0 +1,187 @@
+$(document).ready(function () {
+
+    $('.clearable').each(function () {
+        var $inp = $(this).find('input'),
+            $cle = $(this).find('.clearable__clear');
+
+        $inp.on('input', function () {
+            $cle.toggle(!!this.value);
+        });
+
+        $cle.on('touchstart click', function (e) {
+            e.preventDefault();
+            $inp.val('').trigger('input');
+        });
+    });
+
+    //Show pass
+    $('.show_pass').click(function () {
+        if ($(this).hasClass('hidden') === false) {
+            $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'text');
+            $(this).addClass('hidden');
+        }
+        else {
+            $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'password');
+            $(this).removeClass('hidden');
+        }
+    });
+
+
+    $('.show_pass')
+        .mouseup(function () {
+            $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'password');
+        })
+        .mousedown(function () {
+            $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'text');
+        });
+
+    // OTP input
+    function processInput(holder) {
+        var elements = holder.children(), //taking the "kids" of the parent
+            str = ''; //unnecesary || added for some future mods
+
+        elements.each(function (e) { //iterates through each element
+            var val = $(this).val().replace(/\D/, ''), //taking the value and parsing it. Returns string without changing the value.
+                focused = $(this).is(':focus'), //checks if the current element in the iteration is focused
+                parseGate = false;
+
+            var clear = $('#inputs input');
+            val.length === 1 ? parseGate = false : parseGate = true;
+            /*a fix that doesn't allow the cursor to jump
+            to another field even if input was parsed
+            and nothing was added to the input*/
+
+            $(this).val(val); //applying parsed value.
+
+            if (parseGate && val.length > 1) { //Takes you to another input
+                var exist = elements[e + 1] ? true : false; //checks if there is input ahead
+
+                exist && val[1] ? ( //if so then
+                    elements[e + 1].disabled = false,
+                        elements[e + 1].value = val[1], //sends the last character to the next input
+                        elements[e].value = val[0], //clears the last character of this input
+
+                        elements[e + 1].focus(), //sends the focus to the next input
+                        elements[e + 1].classList.add('focused')
+                ) : void 0;
+            } else if (parseGate && focused && val.length == 0) { //if the input was REMOVING the character, then
+
+                var exist = elements[e - 1] ? true : false; //checks if there is an input before
+                if (exist) elements[e - 1].focus(); //sends the focus back to the previous input
+                elements[e].classList.remove('focused');
+                if (e === 0) {
+                    elements[0].classList.add('focused');
+                }
+            }
+
+            val === '' ? str += ' ' : str += val;
+        });
+    }
+
+    $('#inputs').on('input', function () {
+        processInput($(this))
+    }); //still wonder how it worked out. But we are adding input listener to the parent... (omg, jquery is so smart...);
+
+    $('#inputs').on('click', function (e) { //making so that if human focuses on the wrong input (not first) it will move the focus to a first empty one.
+        let els = $(this).children(),
+            str = '';
+        els.each(function (e) {
+            let focus = $(this).is(':focus');
+            $this = $(this);
+            while ($this.prev().val() === '') {
+                $this.prev().focus();
+                $this = $this.prev();
+            }
+        })
+    });
+
+    //
+    $('.btn_edit_profile').click(function () {
+        $('body').addClass('editmode');
+    });
+
+    $('.btn_un_editmode').click(function () {
+        $('body').removeClass('editmode');
+    });
+
+    //Hidden backgroud when show keyboard
+    let _originalSize = $(window).width() + $(window).height();
+    $(window).resize(function () {
+        if ($(window).width() + $(window).height() !== _originalSize) {
+            $('body').css('background-size', '0');
+        } else {
+            $('body').css('background-size', 'contain');
+        }
+    });
+
+    $(document).on('focus', 'input', function (evt) {
+        $('body').addClass('fix-fixed');
+        $('*').filter(function () {
+            return $(this).css('position') === 'fixed';
+        }).addClass('fix-fixed');
+    });
+
+    $(document).on('blur', 'input', function (evt) {
+        $('body').removeClass('fix-fixed');
+        $('*').filter(function () {
+            return $(this).css('position') === 'fixed';
+        }).removeClass('fix-fixed');
+    });
+
+    // Process custom event pindel
+    $('#inputs input').on('delpin', function (evt) {
+        let curr = parseInt(evt.target.id.substr('pin-'.length));
+        $('#pin-' + curr).val('');
+
+        if (curr >= 0) {
+            $('#pin-' + curr).trigger('focus', $.Event('focus'));
+        }
+    });
+
+    $('#inputs input').focus(function (evt) {
+        let curr = parseInt(evt.target.id.substr('pin-'.length));
+
+        // Check all PIN are empty
+        if ($('#pin-0').val() === '' && curr > 0) {
+            $(evt.target).blur();
+            evt.stopPropagation();
+        }
+
+        let found = false;
+        for (let i = 5; i >= 0; i--) {
+            let digit = $('#pin-' + i).val().trim();
+            //console.log(i + ' = ' + digit);
+            if (digit !== '') {
+                found = i + 1;
+                break;
+            }
+        }
+
+        if (found !== false && found !== curr) {
+            //console.log('found to focus ' + found);
+            $(evt.target).blur();
+            evt.stopPropagation();
+            $('#pin-' + found).trigger('focus', $.Event('focus'));
+        }
+    });
+
+    // Focus on start
+    $('#pin-0').focus();
+
+    // Set height of HTML tag
+    $('html').css('height', $(document).height());
+
+
+    // Auto close popup
+    setTimeout(function () {
+        $('#ShowThanhCong').modal('hide');
+    }, 2000);
+
+    // PIN
+    if ($('#inputs input').length > 0) {
+        $('#inputs input').jqueryPincodeAutotab();
+    }
+
+    //auto focus OTP
+    //setTimeout(function() {$('.focused').focus();}, 1000);
+});
---
+++ jquery-pincode-autotab.js
@@ -0,0 +1,255 @@
+(function ($) {
+    $.fn.jqueryPincodeAutotab = function (options) {
+        var listOfElements = $(this);
+
+        var settings = $.extend({
+            prevElement: null,
+            nextElement: null,
+            defaultFlow: true
+        }, options);
+
+        return this.each(function (index, value) {
+            $(value).on('keydown', function (event) {
+                var move = 0;
+                switch (event.keyCode) {
+                    //number 0
+                    case 48:
+                    case 96:
+                        $(this).val('0');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 1
+                    case 49:
+                    case 97:
+                        $(this).val('1');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 2
+                    case 50:
+                    case 98:
+                        $(this).val('2');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 3
+                    case 51:
+                    case 99:
+                        $(this).val('3');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 4
+                    case 52:
+                    case 100:
+                        $(this).val('4');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 5
+                    case 53:
+                    case 101:
+                        $(this).val('5');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 6
+                    case 54:
+                    case 102:
+                        $(this).val('6');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 7
+                    case 55:
+                    case 103:
+                        $(this).val('7');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 8
+                    case 56:
+                    case 104:
+                        $(this).val('8');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    //number 9
+                    case 57:
+                    case 105:
+                        $(this).val('9');
+                        move = 1;
+                        event.preventDefault();
+                        break;
+
+                    case 8: //backspace
+                    case 46: //delete
+                        $(this).val('');
+                        move = -1;
+                        event.preventDefault();
+                        break;
+
+                    case 9: //tab
+                        if (event.shiftKey) {
+                            move = -1;
+                        } else {
+                            move = 1;
+                        }
+                        event.preventDefault();
+                        break;
+
+                    case 86: //v
+                        if (!(event.ctrlKey || event.metaKey)) {
+                            event.preventDefault();
+                        }
+                        break;
+                    case 13: //enter
+                    case 16: //shift
+                    case 17: //ctrl
+                    case 91: //command in mac
+                        break;
+                    case 229: //android device on chrome always returns 229 keycode
+                        var androidKeyCode = $(this).val();
+                        if ($.isNumeric(androidKeyCode)) {
+                            move = 1;
+                        }
+                        break;
+                    default:
+                        event.preventDefault();
+                }
+
+                for (var i = 0; i < listOfElements.length; i++) {
+                    var prevElement;
+                    var nextElement;
+                    if (i - 1 >= 0) {
+                        prevElement = listOfElements[i - 1];
+                    }
+
+                    if (i + 1 <= listOfElements.length) {
+                        nextElement = listOfElements[i + 1];
+                    }
+
+                    if (listOfElements[i] === this) {
+                        var ele, j;
+                        switch (move) {
+                            case 1:
+                                if (nextElement) {
+                                    $(nextElement).select();
+                                    $(nextElement).focus();
+                                } else {
+                                    if (settings.nextElement) {
+                                        settings.nextElement.select();
+                                        settings.nextElement.focus();
+                                    } else if (settings.defaultFlow) {
+                                        ele = $(':focusable');
+                                        for (j = 0; j < ele.length; j++) {
+                                            if (ele[j] === this) {
+                                                if (ele[j + 1]) {
+                                                    $(ele[j + 1]).select();
+                                                    $(ele[j + 1]).focus();
+                                                }
+
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                                break;
+
+                            case -1:
+                                // Delete PIN
+                                if (prevElement) {
+                                    $(prevElement).select();
+                                    //$(prevElement).focus();
+
+                                    // Custom event
+                                    $(prevElement).trigger({type: 'delpin'});
+                                } else {
+                                    if (settings.prevElement) {
+                                        settings.prevElement.select();
+                                        //settings.prevElement.focus();
+
+                                        // Custom event
+                                        settings.prevElement.trigger({type: 'delpin'});
+                                    } else if (settings.defaultFlow) {
+                                        ele = $(':focusable');
+                                        for (j = 0; j < ele.length; j++) {
+                                            if (ele[j] === this) {
+                                                if (ele[j - 1]) {
+                                                    $(ele[j - 1]).select();
+                                                    //$(ele[j - 1]).focus();
+
+                                                    // Custom event
+                                                    $(ele[j - 1]).trigger({type: 'delpin'});
+                                                }
+
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                break;
+                        }
+                    }
+                }
+            });
+
+            $(value).on('paste', function (event) {
+                event.preventDefault();
+                var clipboardData = event.clipboardData || event.originalEvent.clipboardData || window.clipboardData;
+                var pastedData = clipboardData.getData('text/plain');
+                for (var i = 0; i < listOfElements.length; i++) {
+                    var data = function () {
+                        return pastedData[i];
+                    };
+
+                    $(listOfElements[i]).val(data);
+                }
+            });
+        });
+    };
+
+    function visible(element) {
+        return $.expr.filters.visible(element) && !$(element).parents().addBack().filter(function () {
+            return $.css(this, 'visibility') === 'hidden';
+        }).length;
+    }
+
+    function focusable(element, isTabIndexNotNaN) {
+        let map, mapName, img, nodeName = element.nodeName.toLowerCase();
+        if ('area' === nodeName) {
+            map = element.parentNode;
+            mapName = map.name;
+            if (!element.href || !mapName || map.nodeName.toLowerCase() !== 'map') {
+                return false;
+            }
+
+            img = $('img[usemap=#' + mapName + ']')[0];
+            return !!img && visible(img);
+        }
+        return (/input|select|textarea|button|object/.test(nodeName) ?
+            !element.disabled :
+            'a' === nodeName ?
+                element.href || isTabIndexNotNaN :
+                isTabIndexNotNaN) &&
+            // the element and all of its ancestors must be visible
+            visible(element);
+    }
+
+    $.extend($.expr[':'], {
+        focusable: function (element) {
+            return focusable(element, !isNaN($.attr(element, 'tabindex')));
+        }
+    });
+}(jQuery));
