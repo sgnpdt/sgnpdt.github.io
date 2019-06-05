7874dfeb8c8a8c02d81a28967403b04ed6b9f985
Kieran Dang 鄧英武 <langxangvn@gmail.com>
2019-06-05 10:57:17 +0700

    Fix focus PIN
    kierandg fix focus PIN

--- js/jquery-pincode-autotab.js
+++ js/jquery-pincode-autotab.js
@@ -1,5 +1,5 @@
-(function($) {
-    $.fn.jqueryPincodeAutotab = function(options) {
+(function ($) {
+    $.fn.jqueryPincodeAutotab = function (options) {
         var listOfElements = $(this);
 
         var settings = $.extend({
@@ -8,14 +8,14 @@
             defaultFlow: true
         }, options);
 
-        return this.each(function(index, value) {
-            $(value).on("keydown", function(event) {
+        return this.each(function (index, value) {
+            $(value).on('keydown', function (event) {
                 var move = 0;
                 switch (event.keyCode) {
                     //number 0
                     case 48:
                     case 96:
-                        $(this).val("0");
+                        $(this).val('0');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -23,7 +23,7 @@
                     //number 1
                     case 49:
                     case 97:
-                        $(this).val("1");
+                        $(this).val('1');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -31,7 +31,7 @@
                     //number 2
                     case 50:
                     case 98:
-                        $(this).val("2");
+                        $(this).val('2');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -39,7 +39,7 @@
                     //number 3
                     case 51:
                     case 99:
-                        $(this).val("3");
+                        $(this).val('3');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -47,7 +47,7 @@
                     //number 4
                     case 52:
                     case 100:
-                        $(this).val("4");
+                        $(this).val('4');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -55,7 +55,7 @@
                     //number 5
                     case 53:
                     case 101:
-                        $(this).val("5");
+                        $(this).val('5');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -63,7 +63,7 @@
                     //number 6
                     case 54:
                     case 102:
-                        $(this).val("6");
+                        $(this).val('6');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -71,7 +71,7 @@
                     //number 7
                     case 55:
                     case 103:
-                        $(this).val("7");
+                        $(this).val('7');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -79,7 +79,7 @@
                     //number 8
                     case 56:
                     case 104:
-                        $(this).val("8");
+                        $(this).val('8');
                         move = 1;
                         event.preventDefault();
                         break;
@@ -87,14 +87,14 @@
                     //number 9
                     case 57:
                     case 105:
-                        $(this).val("9");
+                        $(this).val('9');
                         move = 1;
                         event.preventDefault();
                         break;
 
                     case 8: //backspace
                     case 46: //delete
-                        $(this).val("");
+                        $(this).val('');
                         move = -1;
                         event.preventDefault();
                         break;
@@ -134,75 +134,122 @@
                     if (i - 1 >= 0) {
                         prevElement = listOfElements[i - 1];
                     }
+
                     if (i + 1 <= listOfElements.length) {
                         nextElement = listOfElements[i + 1];
                     }
-                    if (listOfElements[i] == this) {
+
+                    if (listOfElements[i] === this) {
+                        var ele, j;
                         switch (move) {
                             case 1:
                                 if (nextElement) {
                                     $(nextElement).select();
                                     $(nextElement).focus();
                                 } else {
-                                    if(settings.nextElement) {
+                                    if (settings.nextElement) {
                                         settings.nextElement.select();
                                         settings.nextElement.focus();
                                     } else if (settings.defaultFlow) {
-                                        var focusable = $(":focusable");
-                                        for (var j = 0; j < focusable.length; j++) {
-                                            if(focusable[j] == this) {
-                                                if(focusable[j + 1]) {
-                                                    $(focusable[j + 1]).select();
-                                                    $(focusable[j + 1]).focus();
+                                        ele = $(':focusable');
+                                        for (j = 0; j < ele.length; j++) {
+                                            if (ele[j] === this) {
+                                                if (ele[j + 1]) {
+                                                    $(ele[j + 1]).select();
+                                                    $(ele[j + 1]).focus();
                                                 }
+
                                                 break;
                                             }
                                         }
                                     }
                                 }
                                 break;
+
                             case -1:
+                                // Delete PIN
                                 if (prevElement) {
                                     $(prevElement).select();
                                     //$(prevElement).focus();
-									
-									$(prevElement).trigger({type: "backFocus"});
+
+                                    // Custom event
+                                    $(prevElement).trigger({type: 'delpin'});
                                 } else {
-                                    if(settings.prevElement) {
+                                    if (settings.prevElement) {
                                         settings.prevElement.select();
                                         //settings.prevElement.focus();
-										settings.prevElement.trigger({type: "backFocus"});
+
+                                        // Custom event
+                                        settings.prevElement.trigger({type: 'delpin'});
                                     } else if (settings.defaultFlow) {
-                                        var focusable = $(":focusable");
-                                        for (var j = 0; j < focusable.length; j++) {
-                                            if(focusable[j] == this) {
-                                                if(focusable[j - 1]) {
-                                                    $(focusable[j - 1]).select();
-                                                    //$(focusable[j - 1]).focus();
-													$(focusable[j - 1]).trigger({type: "backFocus"});
+                                        ele = $(':focusable');
+                                        for (j = 0; j < ele.length; j++) {
+                                            if (ele[j] === this) {
+                                                if (ele[j - 1]) {
+                                                    $(ele[j - 1]).select();
+                                                    //$(ele[j - 1]).focus();
+
+                                                    // Custom event
+                                                    $(ele[j - 1]).trigger({type: 'delpin'});
                                                 }
+
                                                 break;
                                             }
                                         }
                                     }
                                 }
+
                                 break;
                         }
                     }
                 }
             });
 
-            $(value).on("paste", function(event){
+            $(value).on('paste', function (event) {
                 event.preventDefault();
                 var clipboardData = event.clipboardData || event.originalEvent.clipboardData || window.clipboardData;
                 var pastedData = clipboardData.getData('text/plain');
                 for (var i = 0; i < listOfElements.length; i++) {
                     var data = function () {
                         return pastedData[i];
-                    }
+                    };
+
                     $(listOfElements[i]).val(data);
                 }
             });
         });
     };
+
+    function visible(element) {
+        return $.expr.filters.visible(element) && !$(element).parents().addBack().filter(function () {
+            return $.css(this, 'visibility') === 'hidden';
+        }).length;
+    }
+
+    function focusable(element, isTabIndexNotNaN) {
+        let map, mapName, img, nodeName = element.nodeName.toLowerCase();
+        if ('area' === nodeName) {
+            map = element.parentNode;
+            mapName = map.name;
+            if (!element.href || !mapName || map.nodeName.toLowerCase() !== 'map') {
+                return false;
+            }
+
+            img = $('img[usemap=#' + mapName + ']')[0];
+            return !!img && visible(img);
+        }
+        return (/input|select|textarea|button|object/.test(nodeName) ?
+            !element.disabled :
+            'a' === nodeName ?
+                element.href || isTabIndexNotNaN :
+                isTabIndexNotNaN) &&
+            // the element and all of its ancestors must be visible
+            visible(element);
+    }
+
+    $.extend($.expr[':'], {
+        focusable: function (element) {
+            return focusable(element, !isNaN($.attr(element, 'tabindex')));
+        }
+    });
 }(jQuery));
