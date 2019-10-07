52a5ca5010de0366412c525ffbc45f53136ae09e
Kieran Dang 鄧英武 <langxangvn@gmail.com>
2019-10-07 17:37:30 +0700

    Update text

--- js/common.js
+++ js/common.js
@@ -40,21 +40,21 @@ $(document).ready(function () {
         });
 
         // Prevent click navigation, disable anchor click
-//        var nav = $('#fp-nav');
-//        var anchors = $('#menu a');
-//        nav.css('display', 'inline');
-//        anchors.css('pointer-events', '');
-//
-//        document.addEventListener('scroll', function () {
-//            nav.css('display', 'none');
-//            anchors.css('pointer-events', 'none');
-//
-//            // Disable for 600ms
-//            setTimeout(function () {
-//                nav.css('display', 'inline');
-//                anchors.css('pointer-events', '');
-//            }, 1500);
-//        });
+        // var nav = $('#fp-nav');
+        // var anchors = $('#menu a');
+        // nav.css('display', 'inline');
+        // anchors.css('pointer-events', '');
+        //
+        // document.addEventListener('scroll', function () {
+        //     nav.css('display', 'none');
+        //     anchors.css('pointer-events', 'none');
+        //
+        //     // Disable for 600ms
+        //     setTimeout(function () {
+        //         nav.css('display', 'inline');
+        //         anchors.css('pointer-events', '');
+        //     }, 1500);
+        // });
 
         // Back to top
         if (window.location.href.indexOf(guidePage) !== -1) {
@@ -91,20 +91,17 @@ $(document).ready(function () {
         if ($(this).hasClass('hidden') === false) {
             $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'text');
             $(this).addClass('hidden');
-        }
-        else {
+        } else {
             $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'password');
             $(this).removeClass('hidden');
         }
     });
 
-    $('.show_pass')
-        .mouseup(function () {
-            $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'password');
-        })
-        .mousedown(function () {
-            $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'text');
-        });
+    $('.show_pass').mouseup(function () {
+        $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'password');
+    }).mousedown(function () {
+        $(this).parents('.group_form_null ').find('.input_mk').attr('type', 'text');
+    });
 
     // Password complexity indicator
     $('.div_input_pos_signup .input_mk').on('keyup change', function (evt) {
@@ -206,7 +203,7 @@ $(document).ready(function () {
         var elements = holder.children(), // taking the "kids" of the parent
             str = ''; //unnecesary || added for some future mods
 
-        elements.each(function (e) { // iterates through each element
+        elements.each(function (evt) { // iterates through each element
             var val = $(this).val().replace(/\D/, ''); // taking the value and parsing it. Returns string without changing the value.
             var focused = $(this).is(':focus'); // checks if the current element in the iteration is focused
             var clear = $('#inputs input');
@@ -222,31 +219,31 @@ $(document).ready(function () {
             var exist; // checks if there is input
             if (parseGate && val.length > 1) {
                 // takes you to another input
-                exist = elements[e + 1] ? true : false; // checks if there is input ahead
+                exist = elements[evt + 1] ? true : false; // checks if there is input ahead
 
                 if (exist && val[1]) {
                     // if so then
-                    elements[e + 1].disabled = false;
-                    elements[e + 1].value = val[1]; // sends the last character to the next input
-                    elements[e].value = val[0]; // clears the last character of this input
+                    elements[evt + 1].disabled = false;
+                    elements[evt + 1].value = val[1]; // sends the last character to the next input
+                    elements[evt].value = val[0]; // clears the last character of this input
 
 
                     // NEED TO FOCUS HERE
                     //elements[e + 1].focus(); // sends the focus to the next input
-                    setPinFocus(elements[e + 1]);
-                    elements[e + 1].classList.add('focused');
+                    setPinFocus(elements[evt + 1]);
+                    elements[evt + 1].classList.add('focused');
                 }
             } else if (parseGate && focused && val.length === 0) {
                 // if the input was REMOVING the character, then
 
-                exist = elements[e - 1] ? true : false; // checks if there is an input before
+                exist = elements[evt - 1] ? true : false; // checks if there is an input before
                 if (exist) {
-                    elements[e - 1].focus(); // sends the focus back to the previous input
+                    elements[evt - 1].focus(); // sends the focus back to the previous input
                 }
 
-                elements[e].classList.remove('focused');
+                elements[evt].classList.remove('focused');
 
-                if (e === 0) {
+                if (evt === 0) {
                     elements[0].classList.add('focused');
                 }
             }
@@ -407,7 +404,7 @@ $(document).ready(function () {
 
     // Loading icon
     $.getJSON('/master/images/animation-player.json', function () {
-        // do nothing
+        // do nothing here?
     });
 
     $('#ShowLoading').on('show.bs.modal', function (e) {
