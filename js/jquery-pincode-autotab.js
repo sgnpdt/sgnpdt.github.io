96ef82931d1271cd415a11db976fb7ee91126bb7
Kieran Dang 鄧英武 <langxangvn@gmail.com>
2019-10-08 14:46:41 +0700

    Update jquery-pincode-autotab.js

--- js/jquery-pincode-autotab.js
+++ js/jquery-pincode-autotab.js
@@ -178,7 +178,7 @@
 
                     case 229: // Android device on Chrome always returns 229 keycode
                         var androidKeyCode = $(this).val();
-                        $('.pin-log').html('androidKeyCode' + androidKeyCode + '<br />' + $('.pin-log').html());
+                        $('.pin-log').html('androidKeyCode' + androidKeyCode + '/' + $.isNumeric(androidKeyCode) + '<br />' + $('.pin-log').html());
                         if ($.isNumeric(androidKeyCode)) {
                             move = 1;
                         }
